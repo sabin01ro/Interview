16b510eda205649f06a8fc6ff37fee5d1b9956d3
sabin01ro <78943677+sabin01ro@users.noreply.github.com>
2021-02-15 09:40:58 +0000

    Adding Updated script

--- SqlScript/Interview.sql
+++ SqlScript/Interview.sql
@@ -1,31 +1,34 @@
+
 USE [Interview]
 GO
+	BEGIN TRY
+		BEGIN TRANSACTION 
 
-/****** Object:  Table [dbo].[Employees]    Script Date: 12/02/2021 03:04:40 ******/
-SET ANSI_NULLS ON
-GO
+		SET ANSI_NULLS ON
 
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[Employees](
-	[ID] [uniqueidentifier] NOT NULL,
-	[Title] [nvarchar](10) NULL,
-	[First_Name] [nvarchar](50) NULL,
-	[Last_Name] [nvarchar](50) NULL,
-	[Sex] [nvarchar](10) NULL,
-	[Date_Of_Birth] [date] NULL,
-	[Email] [nvarchar](255) NULL,
-	[Job_Title] [nvarchar](255) NULL,
-	[Salary] [float] NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[Employees] ADD  DEFAULT (newid()) FOR [ID]
-GO
+		SET QUOTED_IDENTIFIER ON
+		
+			CREATE TABLE [dbo].[Employees](
+				[ID] [uniqueidentifier] NOT NULL,
+				[Title] [nvarchar](10) NULL,
+				[First_Name] [nvarchar](50) NULL,
+				[Last_Name] [nvarchar](50) NULL,
+				[Sex] [nvarchar](10) NULL,
+				[Date_Of_Birth] [date] NULL,
+				[Email] [nvarchar](255) NULL,
+				[Job_Title] [nvarchar](255) NULL,
+				[Salary] [float] NULL,
+			PRIMARY KEY CLUSTERED 
+			(
+				[ID] ASC
+			)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+			) ON [PRIMARY]
 
 
+			ALTER TABLE [dbo].[Employees] ADD  DEFAULT (newid()) FOR [ID]
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+	END CATCH
+ 
